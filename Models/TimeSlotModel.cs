d4fcd99b1c2ea7385edb4f7fa50059f241c44fff
aldev13 <aldev13@gmail.com>
2021-03-17 19:57:38 -0600

    Name Change to Slots

--- Models/TimeSlotModel.cs
+++ Models/TimeSlotModel.cs
@@ -7,7 +7,7 @@ namespace TempleTours.Models
 {
     public class TimeSlotModel
     {
-        public class Times 
+        public class Slots 
         { 
         public string Time { get; set; }
 
@@ -17,641 +17,641 @@ namespace TempleTours.Models
         
         }
 
-        public List<Times> Schedule { get; set; } = new List<Times>();
+        public List<Slots> Schedule { get; set; } = new List<Slots>();
         
         public TimeSlotModel()
         {
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "8:00 AM",
                 Day = "Sunday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "9:00 AM",
                 Day = "Sunday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "10:00 AM",
                 Day = "Sunday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "11:00AM",
                 Day = "Sunday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "12:00 PM",
                 Day = "Sunday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "1:00 PM",
                 Day = "Sunday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "2:00 PM",
                 Day = "Sunday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "3:00 PM",
                 Day = "Sunday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "4:00 PM",
                 Day = "Sunday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "5:00 PM",
                 Day = "Sunday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "6:00 PM",
                 Day = "Sunday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "7:00 PM",
                 Day = "Sunday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "8:00 PM",
                 Day = "Sunday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "8:00 AM",
                 Day = "Monday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "9:00 AM",
                 Day = "Monday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "10:00 AM",
                 Day = "Monday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "11:00 AM",
                 Day = "Monday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "12:00 PM",
                 Day = "Monday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "1:00 PM",
                 Day = "Monday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "2:00 PM",
                 Day = "Monday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "3:00 PM",
                 Day = "Monday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "4:00 PM",
                 Day = "Monday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "5:00 PM",
                 Day = "Monday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "6:00 PM",
                 Day = "Monday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "7:00 PM",
                 Day = "Monday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "8:00 PM",
                 Day = "Monday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "8:00 AM",
                 Day = "Tuesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "9:00 AM",
                 Day = "Tuesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "10:00 AM",
                 Day = "Tuesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "11:00 AM",
                 Day = "Tuesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "12:00 PM",
                 Day = "Tuesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "1:00 PM",
                 Day = "Tuesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "2:00 PM",
                 Day = "Tuesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "3:00 PM",
                 Day = "Tuesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "4:00 PM",
                 Day = "Tuesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "5:00 PM",
                 Day = "Tuesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "6:00 PM",
                 Day = "Tuesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "7:00 PM",
                 Day = "Tuesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "8:00 PM",
                 Day = "Tuesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "8:00 AM",
                 Day = "Wednesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "9:00 AM",
                 Day = "Wednesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "10:00 AM",
                 Day = "Wednesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "11:00 AM",
                 Day = "Wednesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "12:00 PM",
                 Day = "Wednesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "1:00 PM",
                 Day = "Wednesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "2:00 PM",
                 Day = "Wednesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "3:00 PM",
                 Day = "Wednesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "4:00 PM",
                 Day = "Wednesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "5:00 PM",
                 Day = "Wednesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "6:00 PM",
                 Day = "Wednesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "7:00 PM",
                 Day = "Wednesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "8:00 PM",
                 Day = "Wednesday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "8:00 AM",
                 Day = "Thursday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "9:00 AM",
                 Day = "Thursday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "10:00 AM",
                 Day = "Thursday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "11:00 AM",
                 Day = "Thursday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "12:00 PM",
                 Day = "Thursday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "1:00 PM",
                 Day = "Thursday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "2:00 PM",
                 Day = "Thursday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "3:00 PM",
                 Day = "Thursday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "4:00 PM",
                 Day = "Thursday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "5:00 PM",
                 Day = "Thursday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "6:00 PM",
                 Day = "Thursday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "7:00 PM",
                 Day = "Thursday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "8:00 PM",
                 Day = "Thursday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "8:00 AM",
                 Day = "Friday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "9:00 AM",
                 Day = "Friday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "10:00 AM",
                 Day = "Friday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "11:00 AM",
                 Day = "Friday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "12:00 PM",
                 Day = "Friday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "1:00 PM",
                 Day = "Friday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "2:00 PM",
                 Day = "Friday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "3:00 PM",
                 Day = "Friday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "4:00 PM",
                 Day = "Friday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "5:00 PM",
                 Day = "Friday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "6:00 PM",
                 Day = "Friday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "7:00 PM",
                 Day = "Friday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "8:00 PM",
                 Day = "Friday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "8:00 AM",
                 Day = "Saturday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "9:00 AM",
                 Day = "Saturday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "10:00 AM",
                 Day = "Saturday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "11:00 AM",
                 Day = "Saturday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "12:00 PM",
                 Day = "Saturday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "1:00 PM",
                 Day = "Saturday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "2:00 PM",
                 Day = "Saturday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "3:00 PM",
                 Day = "Saturday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "4:00 PM",
                 Day = "Saturday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "5:00 PM",
                 Day = "Saturday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "6:00 PM",
                 Day = "Saturday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "7:00 PM",
                 Day = "Saturday",
                 Available = true
             });
 
-            Schedule.Add(new Times
+            Schedule.Add(new Slots
             {
                 Time = "8:00 PM",
                 Day = "Saturday",
